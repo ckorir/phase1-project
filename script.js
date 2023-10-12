e8cc7d47572173f7fa56f02fa33b21862a3fff6f
ckorir <chrisruto.k@gmail.com>
2023-10-12 21:26:21 +0300

    Made code cleaner

--- script.js
+++ script.js
@@ -1,152 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     // Get Html Elements
-//     const name = document.querySelector('.name');
-//     const rate = document.querySelector('.rate');
-//     const action = document.querySelector('.action');
-//     const watchName = document.querySelector('.watch-name');
-//     const watchRate = document.querySelector('.watch-rate');
-//     const watchAction = document.querySelector('.watch-action');
-//     const form = document.querySelector('form');
-//     const searchResults = document.querySelector('.search-results');
-
-//     function fetchCurrencyDetails() {
-//         const endpoint = '/api/v3/global';
-//         const url = 'https://api.coingecko.com';
-
-//         fetch(url + endpoint, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             const marketCaps = data.data.total_market_cap;
-//             //console.log(marketCaps);
-
-//             for (const currency in marketCaps) {
-//                 const marketCap = marketCaps[currency];
-//                 // Call the function to add a symbol with a watch event
-//                 addSymbolWithWatch(currency, marketCap);
-//             }
-//         });
-//     }
-//     fetchCurrencyDetails();
-
-//     // Function to add a symbol with a watch event
-//     function addSymbolWithWatch(currency, marketCap) {
-
-//         // Create Symbol Data
-//         const symbolName = document.createElement('p');
-//         const symbolRate = document.createElement('p');
-//         symbolName.textContent = currency;
-//         symbolRate.textContent = marketCap;
-
-//         // Create remove action
-//         const remove = document.createElement('p');
-//         remove.className = 'watching';
-//         const unwatch = document.createElement('a');
-//         unwatch.className = 'watching';
-//         unwatch.textContent = 'Remove';
-//         remove.appendChild(unwatch);
-
-//         // Create watch action 
-//         const favorite = document.createElement('p');
-//         favorite.className = 'watching';
-//         const watch = document.createElement('a');
-//         watch.className = 'watching';
-//         watch.textContent = 'Watch';
-
-//         // Append data to the main section
-//         name.appendChild(symbolName);
-//         rate.appendChild(symbolRate);
-//         favorite.appendChild(watch);
-//         action.appendChild(favorite);
-
-//         // Add a click event to the Watch
-//         favorite.addEventListener('click', () => {
-            
-//             // Move the symbol and market cap to the watchlist
-//             watchName.appendChild(symbolName);
-//             watchRate.appendChild(symbolRate);
-//             favorite.remove();            
-//             watchAction.appendChild(remove);
-
-//         });      
-        
-//         // Add a click event to the unwatch
-//         remove.addEventListener('click', () => {
-
-//             watchName.removeChild(symbolName);
-//             watchRate.removeChild(symbolRate);
-//             remove.remove();
-
-//             addSymbolWithWatch(currency, marketCap)
-        
-//         });
-
-//         // Add a eventlistener to the form
-//         form.addEventListener('submit', (e) => {
-
-//             e.preventDefault();
-//             const searchValue = e.target.search.value;
-//             searchCurrency(searchValue);
-//             //console.log(input)
-
-//             form.reset();
-//         });
-
-//         function searchCurrency(search) {
-//             const searchEndpoint = '/api/v3/coins/markets';
-//             const url = 'https://api.coingecko.com';
-        
-//             // Define the currency name you want to search for
-//             const searchCurrencyName = search;
-        
-//             fetch(`${url}${searchEndpoint}?vs_currency=usd&ids=${searchCurrencyName}`, {
-//                 method: 'GET',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 },
-//             })
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 let searchResultHTML = ''; // Initialize the result HTML
-        
-//                 for (const currencyData of data) {
-//                     const symbol = currencyData.symbol;
-//                     const marketCap = currencyData.market_cap;
-        
-//                     if (currencyData.name.toLowerCase() === searchCurrencyName.toLowerCase()) {
-//                         console.log(`Currency: ${searchCurrencyName}, Symbol: ${symbol}, Market Cap: ${marketCap}`);
-//                         searchResultHTML = `
-//                         <table class="searchlist">
-//                             <tr>
-//                                 <th class="head">Symbol</th>
-//                                 <th>Market Cap</th>
-//                             </tr>
-//                             <td>${symbol}</td>
-//                             <td>${marketCap}</td>
-//                         </table>
-//                         `;
-
-//                         searchResults.innerHTML = searchResultHTML;
-//                         break; // Exit the loop when a match is found
-//                     }
-//                 }
-               
-//             })
-//             .catch((error) => {
-//                 console.error('Error:', error);
-//             });
-//         }
-
-//     }
-
-// });
-
-
 document.addEventListener('DOMContentLoaded', function () {
 
     // Get Html Elements
@@ -163,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const endpoint = '/total_market_cap';
     const url = baseUrl + endpoint;
 
+    // Function to fetch data from the server
     function fetchCurrencyDetails() {
         fetch(url, {
             method: 'GET',
@@ -177,8 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
             currencies.forEach(currency => {
                 const {id, symbol, marketCap } = currency;
                 //console.log(`ID: ${id}, Name: ${name}, Symbol: ${symbol}, Market Cap: ${marketCap}`);
-                // Call the function to add a symbol with a watch event
-                //console.log(currency)
+                // Call the function to add a symbol with a watch even
                 addSymbolWithWatch(currency);
             });
         });
@@ -214,25 +65,18 @@ document.addEventListener('DOMContentLoaded', function () {
         rate.appendChild(symbolRate);
         favorite.appendChild(watch);
         action.appendChild(favorite);
-
-        
+      
         // Add a click event to the Watch
-        favorite.addEventListener('click', () => {
-            
-            // Move the symbol and market cap to the watchlist
-            // watchName.appendChild(symbolName);
-            // watchRate.appendChild(symbolRate);
+        favorite.addEventListener('click', () => {   
+            // Call the addList function
             addList();
             favorite.remove();            
-            // watchAction.appendChild(remove);
-            //console.log(`${currency}: ${marketCap}`);
-
         });
         
         function addList(){
-            watchName.append(symbolName);
-            watchRate.append(symbolRate);
-            watchAction.append(remove);
+            watchName.appendChild(symbolName);
+            watchRate.appendChild(symbolRate);
+            watchAction.appendChild(remove);
         }
         
         // Add a click event to the unwatch
@@ -254,13 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     'Content-Type': 'application/json'
                 }
             })
-            .then(response => {
-                if (response.status === 200) {
-                    console.log('Data deleted successfully');
-                } else {
-                    console.log('Failed to delete data');
-                }
-            })
+            .then(response => response.json())
             .then(data => {
                 console.log(data);
             });
@@ -270,17 +108,10 @@ document.addEventListener('DOMContentLoaded', function () {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             const searchValue = e.target.search.value.toLowerCase();
-            //
             // Call the searchCurrency function
             searchCurrency(searchValue);
-            //form.reset();
-
-
-
-            
         });
-
-            
+   
             function searchCurrency(searchValue){
                 fetch(url, {
                     method: 'GET',
@@ -301,43 +132,35 @@ document.addEventListener('DOMContentLoaded', function () {
                                 <th>Action</th>
                             </tr>
                             <tr>
-                                <td class="line" >${foundCurrency.symbol}</td>
-                                <td class="line" >${foundCurrency.marketCap}</td>
+                                <td class="n1 line">${foundCurrency.symbol}</td>
+                                <td class="n2 line">${foundCurrency.marketCap}</td>
                                 <td class ="add">Watch</td>
                             </tr> 
                         </table>
                         `;
 
                         searchResults.innerHTML = searchResultHTML;
-                        function addToWatch(){
 
+                        function addToWatch(){
                             // Create Symbol Data
                             const add = document.querySelector('.add');
                             add.addEventListener('click', () =>{
                                 symbolName.textContent = foundCurrency.symbol;
                                 symbolRate.textContent = foundCurrency.marketCap;
+                                // Call addList function
                                 addList();
-                                //favorite.remove();            
-                                //watchAction.appendChild(remove);
-                            });
-                            
+                            });  
                         }
                         addToWatch();
                     } else {
-                        searchResults.innerHTML = "Currency not found.";
+                        searchResults.innerHTML = `
+                        <h5 style="margin-left: 2em; padding: 1em;">Currency not found.</h5>
+                        `;
                     }
                 })
                 .catch(error => {
                     console.error('Error:', error);
                 });
-                //form.reset();
             }
-            
-        
-
-        // function searchCurrency(search) {
-        //     fetch(url,)
-        // }
     }
-
 })
